e20de4be69360a98256ce369d1ece3e75e2cc69e
GeekBrah <khoii87@gmail.com>
2018-10-17 15:25:00 +0200

    Opprettet alle klassene
    - RettLinje
    - Rektangel
    - Sirkel
    - Polygon

---
+++ Obligatorisk Oppgave/Obligatorisk Oppgave/src/obligatorisk/oppgave/App.java
@@ -0,0 +1,25 @@
+package obligatorisk.oppgave;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
---
+++ Obligatorisk Oppgave/Obligatorisk Oppgave/src/obligatorisk/oppgave/Polygon.java
@@ -0,0 +1,5 @@
+package obligatorisk.oppgave;
+
+public class Polygon {
+    
+}
